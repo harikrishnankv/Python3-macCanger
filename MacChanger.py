b6cf1d1e6abe72403b19852b45e0059b967292e9
HARI <harikrishnnkv@gmail.com>
2019-06-12 10:10:52 +0530

    Create MacChanger.py

---
+++ MacChanger.py
@@ -0,0 +1,24 @@
+print("coded by hari")
+import subprocess
+
+print('WELCOME TO MAC CHANGER ')
+print('')
+
+interface=input('Enter the interface (eth0,wlan0,wlan1.....):  ')
+print('')
+fake_MAC=input('Enter fake MAC id that you want to assign(XX:XX:XX:XX:XX:XX): ')
+
+print('current MAC id is : ')
+subprocess.call("ifconfig "+interface,shell=True)
+print('')
+
+subprocess.call("ifconfig "+interface+" down",shell=True)
+
+subprocess.call("ifconfig "+interface+" hw ether "+fake_MAC,shell=True)
+
+subprocess.call("ifconfig "+interface+" up",shell=True)
+
+print('NEW MAC ADDRESS is '+fake_MAC)
+
+print('')
+subprocess.call("ifconfig "+interface,shell=True)
